0f996196cf8438e630c50630feeea67e31a4e8d4
BugCatcherNate <natetthompson@gmail.com>
2022-11-23 18:58:54 -0800

    format

--- src/lib.rs
+++ src/lib.rs
@@ -1,27 +1,21 @@
-
-use csv::{Reader};
+use csv::Reader;
 use serde::Deserialize;
 #[derive(Debug, Deserialize)]
 pub struct Record {
-   pub id: i32,
+    pub id: i32,
     pub solvent: String,
-   pub  d_d: f32,
+    pub d_d: f32,
     pub d_p: f32,
-   pub  d_h: f32
-
+    pub d_h: f32,
 }
 
-
-pub fn distance(a: &Record, b: &Record) -> f32{
-
-        
-    let num = (b.d_d-a.d_d).powi(2) + (b.d_p-a.d_p).powi(2) + (b.d_h-a.d_h).powi(2);  
+pub fn distance(a: &Record, b: &Record) -> f32 {
+    let num = 4.0 * (b.d_d - a.d_d).powi(2) + (b.d_p - a.d_p).powi(2) + (b.d_h - a.d_h).powi(2);
     let res = num.sqrt();
     res
 }
 
-pub fn read_solvents(path: String) -> Vec<Record>{
-
+pub fn read_solvents(path: String) -> Vec<Record> {
     let mut reader = Reader::from_path(path).unwrap();
     let mut solvents: Vec<Record> = Vec::new();
     for result in reader.deserialize() {
@@ -30,4 +24,3 @@ pub fn read_solvents(path: String) -> Vec<Record>{
     }
     solvents
 }
-
--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,18 @@
-use hansen::{Record, read_solvents, distance}; 
+use hansen::{distance, read_solvents, Record};
 
 fn main() {
-
     let solvs: Vec<Record> = read_solvents("solvents.csv".to_string());
     let mut closest: f32 = f32::MAX;
-    for solvent_a in &solvs { 
-
+    for solvent_a in &solvs {
         let solvent_a: &Record = solvent_a;
-        for solvent_b in &solvs{
-
-        let solvent_b: &Record = solvent_b;
-        let c: f32 = distance(solvent_a, solvent_b); 
-        if closest < c{
-            closest = c;
-        }
+        for solvent_b in &solvs {
+            let solvent_b: &Record = solvent_b;
+            let c: f32 = distance(solvent_a, solvent_b);
+            if closest < c {
+                closest = c;
+            }
         }
     }
 
-        println!("{:?}", closest);
+    println!("{:?}", closest);
 }
